5127138bf5c216a020f344347d2cc29fe8f42e24
Franco-RD <franco.romano.data@gmail.com>
2023-11-29 12:37:04 -0300

    Agregados marcadores y colisiones de la pelota contra bordes

--- figura_class.py
+++ figura_class.py
@@ -1,14 +1,12 @@
 import pygame as pg
 
 class Raqueta:
-    def __init__(self, pos_x, pos_y, color=(255,255,255), w=20, h=120, vx=1, vy=1):
+    def __init__(self, pos_x, pos_y, color=(255,255,255), w=20, h=120):
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.w = w
         self.h = h
-        self.vx = vx
-        self.vy = vy
     
     def dibujar(self, surface):
         pg.draw.rect(surface, self.color, (self.pos_x-(self.w//2), self.pos_y-(self.h//2), self.w, self.h))  #Las divisiones en la posicion del draw son para que se dibuje en el medio de la pantalla
@@ -24,12 +22,38 @@ class Raqueta:
 
 
 class Pelota:
-    def __init__(self, pos_x, pos_y, color=(255,255,255), radio=5) -> None:
+    def __init__(self, pos_x, pos_y, color=(255,255,255), radio=5, vx=1, vy=1) -> None:
         self.pos_x = pos_x
         self.pos_y = pos_y
         self.color = color
         self.radio = radio
+        self.vx = vx
+        self.vy = vy
+        self.contadorDerecho = 0
+        self.contadorIzquierdo = 0
     
     def dibujar(self, surface):
         pg.draw.circle(surface, self.color, (self.pos_x, self.pos_y), self.radio)
+
+    def mover(self, x_max=800, y_max=600):
+        self.pos_x += self.vx
+        self.pos_y += self.vy
+
+        #Colision limite derecho
+        if self.pos_x >= x_max+self.radio*10:  #El radio se multiplica por numeros mas grandes que 2 para que la pelota se pierda por mas tiempo fuera de la pantalla tras meter un gol
+            self.pos_x = x_max//2
+            self.pos_y = y_max//2
+            self.vx *= -1
+            self.contadorDerecho += 1
+        
+        #Colision limite izquierdo
+        if self.pos_x <= 0-self.radio*10: 
+            self.pos_x = x_max//2
+            self.pos_y = y_max//2
+            self.vx *= -1
+            self.contadorIzquierdo += 1
+
+        #Colision vertical
+        if self.pos_y >= y_max or self.pos_y <=  0 :
+            self.vy *= -1
         
--- main.py
+++ main.py
@@ -9,9 +9,18 @@ pg.display.set_caption("PONG")
 
 tasa_refresco = pg.time.Clock()  #Definir tasa de refresco en nuestro bucle de fotogramas. 
 
+
+#Agregar marcadores
+#Fuente y tamaño de letra
+marcador1_font = pg.font.SysFont("verdana", 30)  #SysFont toma parametros del sistema
+marcador2_font = pg.font.SysFont("verdana", 30)
+jugador1_font = pg.font.SysFont("verdana", 10,True)
+jugador2_font = pg.font.SysFont("verdana", 10,True)
+
+
+
 #Definicion de objetos para el juego
 pelota = Pelota(400, 300, (228,231,19), 10)
-
 raqueta1 = Raqueta(10, 300)  #Raqueta izquierda
 raqueta2 = Raqueta(790, 300)  #raqueta derecha
 
@@ -25,6 +34,7 @@ while game_over:
     valor_tasa = tasa_refresco.tick(600)  #Variable para controlar la velocidad entre fotogramas. A menor numero entre () mas lento es
     #print(valor_tasa)
 
+
     #Captura de eventos
     for evento in pg.event.get():
         if evento.type == pg.QUIT:
@@ -34,13 +44,25 @@ while game_over:
     #Seteo de pantalla, dibujo y movimiento de objetos
     pantalla_principal.fill((27,149,47))
     pg.draw.line(pantalla_principal, (255, 255, 255), (400,0), (400, 600), 15)
-
-    raqueta1.mover(pg.K_w, pg.K_s)
-    raqueta2.mover(pg.K_UP, pg.K_DOWN)
-
     pelota.dibujar(pantalla_principal)
     raqueta1.dibujar(pantalla_principal)
     raqueta2.dibujar(pantalla_principal)
+
+    raqueta1.mover(pg.K_w, pg.K_s)
+    raqueta2.mover(pg.K_UP, pg.K_DOWN)
+    pelota.mover()
+    
+
+    #Color del texto
+    marcador1 = marcador1_font.render(str(pelota.contadorDerecho), True, (255,255,255))  #El .render  devuelve un objeto de tipo surface, que muestra un str seteado con demas parametros
+    marcador2 = marcador2_font.render(str(pelota.contadorIzquierdo), True, (255,255,255))
+    jugador1 = jugador1_font.render("Jugador 1", True, (255,255,255))
+    jugador2 = jugador2_font.render("Jugador 2", True, (255,255,255))
+    #Muestra de marcadores
+    pantalla_principal.blit(jugador1, (310,10))
+    pantalla_principal.blit(jugador2, (430,10))
+    pantalla_principal.blit(marcador1, (330,20))  #Blit muestra un objeto de tipo surface en una coordenada
+    pantalla_principal.blit(marcador2, (450,20))
 
     pg.display.flip()
 
